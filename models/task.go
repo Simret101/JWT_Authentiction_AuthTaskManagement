526c1457c29f5a3192ab772d37830cf6ea1646b2
Simret Belete <104854158+Simret101@users.noreply.github.com>
2024-08-07 15:25:21 +0300

    Add files via upload

---
+++ models/task.go
@@ -0,0 +1,9 @@
+package models
+
+type Task struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	DueDate     string `json:"duedate"`
+	Status      string `json:"status"`
+}
