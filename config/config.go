45583f7b09cf770b30be09305ebc23eda3db818a
Simret Belete <104854158+Simret101@users.noreply.github.com>
2024-08-07 15:23:52 +0300

    Add files via upload

---
+++ config/config.go
@@ -0,0 +1,11 @@
+package config
+
+import "time"
+
+var (
+	SecretKey       = "my_secret_key"
+	TokenExpiration = 1 * time.Hour
+	DatabaseURI     = "mongodb://localhost:27017"
+	UserDBName      = "userdb"
+	TaskDBName      = "taskdb"
+)
