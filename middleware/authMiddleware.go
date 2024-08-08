2524860769aef5f493cd9f312f3364a8ae47a414
Simret Belete <104854158+Simret101@users.noreply.github.com>
2024-08-08 11:03:04 +0300

    Add files via upload

---
+++ middleware/authMiddleware.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+	"task/data"
+
+	"github.com/gin-gonic/gin"
+)
+
+// checks for the presence of an "Authorization" header in the incoming request
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
+			c.Abort()
+			return
+		}
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		claims, err := data.ValidateToken(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			c.Abort()
+			return
+		}
+		c.Set("username", claims.Username)
+		c.Next()
+	}
+}
