6517232640c6d561ac882b918ad4a9aea513b4b9
Simret Belete <104854158+Simret101@users.noreply.github.com>
2024-08-10 15:47:07 +0300

    Update task.go
    Signed-off-by: Simret Belete <104854158+Simret101@users.noreply.github.com>

--- models/task.go
+++ models/task.go
@@ -2,11 +2,7 @@ package models
 
 
 type Task struct {
-	ID          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	DueDate     string `json:"duedate"`
-	Status      string `json:"status"`
+	
 	ID     int    `json:"id"`
 	Title  string `json:"title"`
 	Status string `json:"status"`
