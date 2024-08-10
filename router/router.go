5bbb5fe72139e7e3965d96e5577d00338810a950
Simret Belete <104854158+Simret101@users.noreply.github.com>
2024-08-10 08:34:36 +0300

    Update router.go

--- router/router.go
+++ router/router.go
@@ -7,34 +7,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// sets up the router
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
-	// Public routes
 	r.POST("/register", controllers.Register)
 	r.POST("/login", controllers.Login)
 
-	// Protected routes
-	auth := r.Group("/")
-	auth.Use(middleware.AuthMiddleware())
+	authorized := r.Group("/")
+	authorized.Use(middleware.Auth())
 	{
-		auth.GET("/tasks", controllers.GetAllTasks)
-		auth.GET("/tasks/:id", controllers.GetTaskByID)
-		auth.POST("/tasks", controllers.CreateTask)
-		auth.PUT("/tasks/:id", controllers.UpdateTask)
-		auth.DELETE("/tasks/:id", controllers.DeleteTask)
-	}
-
-	// Admin routes
-	admin := r.Group("/admin")
-	admin.Use(middleware.AuthMiddleware(), middleware.AdminMiddleware())
-	{
-		admin.GET("/tasks", controllers.GetAllTasks)
-		admin.GET("/tasks/:id", controllers.GetTaskByID)
-		admin.POST("/tasks", controllers.CreateTask)
-		admin.PUT("/tasks/:id", controllers.UpdateTask)
-		admin.DELETE("/tasks/:id", controllers.DeleteTask)
+		authorized.GET("/tasks", controllers.GetAllTasks)
+		authorized.GET("/tasks/:id", controllers.GetTaskByID)
+		authorized.POST("/tasks", controllers.CreateTask)
+		authorized.PUT("/tasks/:id", controllers.UpdateTask)
+		authorized.DELETE("/tasks/:id", controllers.DeleteTask)
 	}
 
 	return r
