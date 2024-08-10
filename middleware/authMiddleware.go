0328aa585c488901140e1bff406f9c200d609e28
Simret Belete <104854158+Simret101@users.noreply.github.com>
2024-08-10 08:33:12 +0300

    Update auth_middleware.go

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -3,13 +3,13 @@ package middleware
 import (
 	"net/http"
 	"strings"
-	"task/data"
+	"task/config"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 )
 
-// checks for the presence of an "Authorization" header in the incoming request
-func AuthMiddleware() gin.HandlerFunc {
+func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
@@ -17,14 +17,26 @@ func AuthMiddleware() gin.HandlerFunc {
 			c.Abort()
 			return
 		}
+
 		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
-		claims, err := data.ValidateToken(tokenString)
-		if err != nil {
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, jwt.ErrSignatureInvalid
+			}
+			return []byte(config.SecretKey), nil
+		})
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			c.Set("userID", int(claims["userID"].(float64)))
+			c.Set("role", claims["role"].(string))
+		} else {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 			c.Abort()
 			return
 		}
-		c.Set("username", claims.Username)
+
 		c.Next()
 	}
 }
+
