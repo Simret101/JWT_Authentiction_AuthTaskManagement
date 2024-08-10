efaf5a812ebdfe26045ea1f51248ce73a62c0353
Simret Belete <104854158+Simret101@users.noreply.github.com>
2024-08-10 08:33:45 +0300

    Update task.go

--- models/task.go
+++ models/task.go
@@ -1,10 +1,9 @@
 package models
 
-//defines the task model
 type Task struct {
-	ID          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	DueDate     string `json:"duedate"`
-	Status      string `json:"status"`
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
+	UserID int    `json:"userID"`
 }
+
