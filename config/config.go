e918bd99eb322e237b23043921de4a0a7f30d8e1
Simret Belete <104854158+Simret101@users.noreply.github.com>
2024-08-10 16:01:50 +0300

    Update config.go
    Signed-off-by: Simret Belete <104854158+Simret101@users.noreply.github.com>

--- config/config.go
+++ config/config.go
@@ -2,14 +2,12 @@ package config
 
 import "time"
 
+
 var (
+	SecretKey       = "my_secret_key"
 	TokenExpiration = 1 * time.Hour
 	DatabaseURI     = "mongodb://localhost:27017"
 	UserDBName      = "userdb"
 	TaskDBName      = "taskdb"
 )
 
-const (
-	ServerPort = ":8080"
-	SecretKey  = "your_secret_key"
-)
