d68181e9f2781db77cfb6cd6286240d5870594d6
Simret Belete <104854158+Simret101@users.noreply.github.com>
2024-08-11 06:03:41 +0300

    Update user_controller.go
    Signed-off-by: Simret Belete <104854158+Simret101@users.noreply.github.com>

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -8,20 +8,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
 func Register(c *gin.Context) {
 	var user models.User
 	if err := c.ShouldBindJSON(&user); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
 		return
 	}
+
+	if err := user.Validate(); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
 	if err := data.CreateUser(&user); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
 	c.JSON(http.StatusCreated, user)
 }
-
 
 func Login(c *gin.Context) {
 	var credentials models.Credentials
@@ -29,11 +34,19 @@ func Login(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
 		return
 	}
+
+	if err := credentials.Validate(); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
 	token, err := data.AuthenticateUser(credentials.Username, credentials.Password)
 	if err != nil {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 		return
 	}
+
 	c.JSON(http.StatusOK, gin.H{"token": token})
 }
+
 
